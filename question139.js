8750c25a4c0817b32d800d37d29db7bc04b4e792
amnakifayat13 <hayatullahamna@gmail.com>
2024-04-18 08:30:10 +0500

    Day 47 Challenge

---
+++ question139.js
@@ -0,0 +1,9 @@
+/* List three reserved words in JavaScript and create a valid use case for each.*/
+var count = 5;
+if (count > 0) {
+    console.log("Greater than the 0");
+}
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 6));
---
+++ question139.ts
@@ -0,0 +1,14 @@
+/* List three reserved words in JavaScript and create a valid use case for each.*/
+
+
+let count = 5; // reserved word is let.
+
+if(count > 0) {  // reserved word is if
+    console.log("Greater than the 0");
+    
+}
+
+function add(val1:number,val2: number){
+    return val1+val2; // reserved word is return.
+}
+console.log(add(5,6));
